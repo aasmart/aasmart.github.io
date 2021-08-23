13491720d83700fcfa16c6c9e39c1a59fec4e954
IAmSpecial <48771274+IAmSpecial@users.noreply.github.com>
2021-08-23 17:07:16 -0400

    Removed hover effect. Introduced changes for scrolling

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-window.onload = new function () {
-    const introSubContainer = document.getElementById('intro-sub-container')
+window.onload = () => {
+    let fadeInCount = 0;
+    let introContainer = document.getElementById("intro-sub-container")
 
-    if (introSubContainer) {
-        introSubContainer.addEventListener('click', introClick)
-    }
+    document.addEventListener("animationend", (event) => {
+        if(event.animationName === "float-up")
+            introContainer.style.transform = "translate(0, -37px)"
+        if(event.animationName === "fade-in")
+            fadeInCount++
+        if(fadeInCount === 2) {
+            fadeInCount++
+            let sheet = document.styleSheets[0]
+            let rules = sheet.cssRules || sheet.rules;
+            // rules[2].style.overflowY = "visible"
+        }
+    })
 
-    function introClick() {
+    document.addEventListener("scroll",() => {
+        const scrollHeight = document.documentElement.scrollHeight
+        const maxPercent = 300;
+        const opacity =  1 - (document.documentElement.scrollTop / maxPercent);
+        const height = Math.min(document.documentElement.scrollTop / maxPercent, 1);
 
-    }
+        let introContainer = document.getElementById("intro-sub-container")
+        introContainer.style.transform = `translateY(-${(height * 270) + 37}px) scale(${Math.max(opacity, .75)})`
+
+        let subtitle = document.getElementById("subtitle")
+        subtitle.style.animationFillMode = "none"
+        subtitle.style.opacity = opacity.toString()
+    });
 }
