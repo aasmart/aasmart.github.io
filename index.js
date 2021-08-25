df23f27ddb8298223df84abfc99629a1d8f8a3d9
IAmSpecial <48771274+IAmSpecial@users.noreply.github.com>
2021-08-24 21:15:36 -0400

    Scrolling experiment

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
+window.onbeforeunload = () => {
+    window.scrollTo(0, 0)
+}
+
 window.onload = () => {
     let fadeInCount = 0;
-    let introContainer = document.getElementById("intro-sub-container")
+    let introContainer = document.getElementById("intro-sub-container");
 
     document.addEventListener("animationend", (event) => {
         if(event.animationName === "float-up")
-            introContainer.style.transform = "translate(0, -37px)"
+            introContainer.style.transform = "translate(0, -37px)";
         if(event.animationName === "fade-in")
-            fadeInCount++
+            fadeInCount++;
         if(fadeInCount === 2) {
-            fadeInCount++
-            let sheet = document.styleSheets[0]
+            fadeInCount++;
+            let sheet = document.styleSheets[0];
             let rules = sheet.cssRules || sheet.rules;
-            rules[2].style.overflowY = "visible"
+            rules[2].style.overflowY = "visible";
         }
     })
 
     document.addEventListener("scroll",() => {
         const scrollHeight = (document.documentElement.scrollHeight - window.innerHeight);
-        console.log(scrollHeight)
         const maxPercent = 300;
         const opacity =  1 - (document.documentElement.scrollTop / Math.min(maxPercent, scrollHeight));
         const height = Math.min(document.documentElement.scrollTop / Math.min(maxPercent, scrollHeight), 1);
 
-        let introContainer = document.getElementById("intro-sub-container")
-        introContainer.style.transform = `translateY(-${(height * 270) + 37}px) scale(${Math.max(opacity, .75)})`;
+        // Styling for the header
+        let header = document.getElementById("header");
+        header.style.opacity = (1-opacity >= .7 ? ((1-opacity-.7)*10) : 0).toString();
 
-        let subtitle = document.getElementById("subtitle")
-        subtitle.style.animationFillMode = "none"
-        subtitle.style.opacity = opacity.toString()
+        // Styling for the intro container
+        let introContainer = document.getElementById("intro-sub-container");
+        introContainer.style.transform = `translateY(-${(height * Math.round(window.innerHeight * .335)) + 37}px) scale(${Math.max(opacity, .65)})`;
+
+        // Styling for the subtitle
+        let subtitle = document.getElementById("subtitle");
+        subtitle.style.animationFillMode = "none";
+        subtitle.style.opacity = opacity.toString();
     });
 }
