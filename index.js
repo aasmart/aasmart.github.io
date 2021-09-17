c6e1ea7450d386d149551a71f79cf8cfa9d3bc6f
IAmSpecial <48771274+IAmSpecial@users.noreply.github.com>
2021-09-16 20:34:13 -0400

    Change test message

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ window.onload = () => {
     window.addEventListener("orientationchange", () => {
         windowWidth = screen.innerWidth;
         introContainerTop = windowWidth * containerTopRatio;
-        alert(`Test 1.1 ${window.innerWidth}`)
+        alert(`Test 1.2 ${window.innerWidth}`)
         introContainerScrolling(introContainer, introContainerTop, windowWidth, idealWidth);
         headerBannerHeight(header, windowWidth, idealWidth)
     });
