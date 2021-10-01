02a97fad8bc17ac5929eee32e9b2041b3a53a57c
IAmSpecial <48771274+IAmSpecial@users.noreply.github.com>
2021-09-30 21:33:59 -0400

    Introduce about me page

--- index.js
+++ index.js
@@ -6,15 +6,50 @@ const sheet = document.styleSheets[0];
 const rules = sheet.cssRules || sheet.rules;
 
 window.onload = () => {
-    // Style elements
-    let introContainer = document.getElementById("intro-sub-container");
-    let scrollDown = document.getElementById("scroll-down");
+    if(window.location.href.includes("index.html")) {
+        // Style elements
+        let introContainer = document.getElementById("intro-sub-container");
+        let scrollDown = document.getElementById("home-page");
 
-    scrollDown.addEventListener("click", () => {
-        introContainer.style.animation = "explode .5s forwards";
-        scrollDown.style.animation = "fade-out .5s forwards";
-        setTimeout(() => {
-            window.location = "home.html";
-        }, 350);
-    });
+        scrollDown.addEventListener("click", () => {
+            introContainer.style.animation = "shrink .5s forwards";
+            scrollDown.style.animation = "fade-out .5s forwards";
+            setTimeout(() => {
+                window.location.href = "home.html";
+            }, 350);
+        });
+    } else {
+        const pictures = ["Images/dog1.jpg", "Images/dog2.jpg", "Images/dog3.jpg"];
+        let imageNum = 1;
+        let isTop = true;
+        window.addEventListener("animationiteration", (event) => {
+            let target = event.target;
+            if(target.className.includes("dog-top")) {
+                let dogBottom = document.getElementsByClassName("dog-bottom")[0];
+                imageNum++;
+                if(imageNum >= pictures.length)
+                    imageNum = 0;
+                if(isTop) {
+                    target.src = pictures[imageNum];
+                    target.classList.remove(target.classList[1]);
+                    target.classList.add(`dog-${imageNum+1}`);
+                } else {
+                    dogBottom.src = pictures[imageNum];
+                    dogBottom.classList.remove(dogBottom.classList[1]);
+                    dogBottom.classList.add(`dog-${imageNum+1}`);
+                }
+                isTop = !isTop;
+            }
+        })
+    }
+
+    cycleDogPicture();
+}
+
+function cycleDogPicture() {
+    let dogBottom = document.getElementsByClassName("dog-bottom");
+    let dogTop = document.getElementsByClassName("dog-top");
+
+    dogTop[0].style.animation = "fade-out-delay 10s 4.5s infinite alternate";
+    dogBottom[0].style.animation = "fade-in-delay 10s 4.5s infinite alternate";
 }
